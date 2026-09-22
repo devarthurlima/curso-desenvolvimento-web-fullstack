81ed7a7a405ab03ce6aaa54c92be68b7eca48082
arthur.lima3@icloud.com <arthur.lima3@icloud.com>
2026-09-22 18:13:39 -0300

    feat: Introdução ao JavaScript.

---
+++ Módulo 2 - Desenvolvimento Front-end/HelloWorld/script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Seleciona o elemento HTML com o id 'mensagem'
+const mensagemElemento = document.getElementById("mensagem");
+
+// Modifica o texto do elemento
+mensagemElemento.textContent =
+  "Olá, Mundo! Este é meu primeiro Hello World com JavaScript!";
