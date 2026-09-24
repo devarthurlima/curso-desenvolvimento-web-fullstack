9d8ed58c851951224eff227740fd85d417c08c6a
arthur.lima3@icloud.com <arthur.lima3@icloud.com>
2026-09-24 17:53:52 -0300

    feat: estruturas condicionais.

---
+++ Módulo 2 - Desenvolvimento Front-end/Estrutura Condicional/script.js
@@ -0,0 +1,18 @@
+let mode = "dark";
+
+console.log(mode == "light" ? "É" : "Não é");
+
+// switch(mode) {
+//     case "dark":
+//         document.querySelector("body").style.backgroundColor = "#222222"
+//         break
+//     case "red":
+//         document.querySelector("body").style.backgroundColor = "#ff0000"
+//         break
+//     case "blue":
+//         document.querySelector("body").style.backgroundColor = "#0000ff"
+//         break
+//     default:
+//         document.querySelector("body").style.backgroundColor = "#ffffff"
+//         break
+// }
